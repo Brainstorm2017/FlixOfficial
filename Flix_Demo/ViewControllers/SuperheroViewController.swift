f310e3aa4696da07ba8069539b34ae075a190f5f
user144166 <user144166@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2018-10-29 16:15:51 -0400

    flicks part 2

--- Flix_Demo/ViewControllers/SuperheroViewController.swift
+++ Flix_Demo/ViewControllers/SuperheroViewController.swift
@@ -19,6 +19,16 @@ class SuperheroViewController: UIViewController,   UICollectionViewDataSource {
         super.viewDidLoad()
 
         collectionView.dataSource = self
+        
+        let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        layout.minimumInteritemSpacing = 5
+        layout.minimumLineSpacing = layout.minimumInteritemSpacing
+        let cellsPerLine: CGFloat = 2
+        let interItemSpacingTotal = layout.minimumInteritemSpacing * (cellsPerLine - 1)
+        let width = collectionView.frame.size.width / cellsPerLine - interItemSpacingTotal / cellsPerLine
+        
+        layout.itemSize = CGSize(width: width, height: width * 3 / 2)
+        
         fetchMovies()
     }
     
@@ -32,12 +42,12 @@ class SuperheroViewController: UIViewController,   UICollectionViewDataSource {
         if let posterPathString = movie["poster_path"] as? String {
             let baseURLString = "https://image.tmdb.org/t/p/w500"
             let posterURL = URL(string: baseURLString + posterPathString)!
-            cell.posterImageView.af_setImage(withURL: posterURL)
+            cell.posterimageView.af_setImage(withURL: posterURL)
         }
         return cell
     }
     
-    unc fetchMovies() { let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+    func fetchMovies() { let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
     
     let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
     let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
@@ -67,4 +77,5 @@ class SuperheroViewController: UIViewController,   UICollectionViewDataSource {
     }
     */
 
+}
 }
